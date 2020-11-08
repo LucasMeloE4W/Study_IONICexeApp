562f8272ce27fa4de73888b3d95c9ec80ee1b5a4
LucasMeloE4W <60780318+LucasMeloE4W@users.noreply.github.com>
2020-11-07 21:33:31 -0300

    Add mais exemplos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public resultado: String = "";
+  public pesquisa: String = "";
+  public tituloVariavel: String = "App Estudos";
+  public imgVariavel: String = "https://source.unsplash.com/random/200x200";
+  
+  constructor(private navegacao: NavController){ }
+
+  ngOnInit(){
+    
+  }
+
+  recuperar(){
+    this.recuperar().pesquisa;
+  }
+
+  abrirBotoes(){
+    this.navegacao.navigateForward('botoes')
+  }
+
+  // -------------- !! ------------- \\
+  public atualizaDados():void {
+
+    this.tituloVariavel = "Titulo Alterado"
+  }
+
+  public acaoBotao():void {
+
+    this.tituloVariavel = "Botão Clicado"
+  }
+  // -------------- !! ------------- \\
 
 }
